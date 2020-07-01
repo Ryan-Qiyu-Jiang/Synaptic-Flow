eecf8e775c65a0cb33a1845c1868dc467e01a7e3
Ryan Jiang <rq2jiang@edu.uwaterloo.ca>
2020-06-30 20:02:17 -0400

    break instead of quit when too few prunable params

--- prune.py
+++ prune.py
@@ -22,4 +22,3 @@ def prune_loop(model, loss, pruner, dataloader, device,
     remaining_params, total_params = pruner.stats()
     if np.abs(remaining_params - total_params*sparsity) >= 1:
         print("ERROR: {} prunable parameters remaining, expected {}".format(remaining_params, total_params*sparsity))
-        quit()
